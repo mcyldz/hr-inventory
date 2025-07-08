488d8e961b2182f66cdc799b44b14ffee65709a5
mcyldz <116563621+mcyldz@users.noreply.github.com>
2025-07-08 13:48:54 +0300

    HR Inventory

---
+++ src/main/java/com/mcyldz/hrinventory/HrInventoryApplication.java
@@ -0,0 +1,13 @@
+package com.mcyldz.hrinventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrInventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrInventoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mcyldz/hrinventory/HrInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcyldz.hrinventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrInventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
